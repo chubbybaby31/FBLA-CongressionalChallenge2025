6be057241e666ef8514098a2998be43dc8f0887c
AarushKej <114349791+AarushKej@users.noreply.github.com>
2024-11-13 22:40:07 -0500

    jsonify

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from datetime import datetime
 import os
 import plotly
